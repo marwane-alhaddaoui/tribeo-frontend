f6fe1bfa47979b61d5cd8a9516c38b7425206e05
Marwane Al haddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-08-17 04:03:44 +0200

    fix-config-deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",    // ⚡ important pour Render
   plugins: [react()],
+  base: '/',                 // <-- IMPORTANT
+  build: { outDir: 'dist', assetsDir: 'assets' }
 })
